159f04854822f10cb6d08b1e524a9ae29c80cf12
Mayank Aitan <aitanmayank@gmail.com>
2025-09-10 20:08:18 +0530

    vite configed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
   plugins: [react()],
   resolve: {
     alias: {
@@ -15,7 +17,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: env.VITE_API_URL,
         changeOrigin: true,
       },
     },
@@ -36,4 +38,5 @@ export default defineConfig({
       },
     },
   },
+}
 })
